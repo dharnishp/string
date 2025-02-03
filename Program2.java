91dafba3b9b0ae047587bddf5d9d9f8dae4d6a81
dharnishp <128043459+dharnishp@users.noreply.github.com>
2025-02-03 12:32:17 +0530

    Create Program2.java

---
+++ Program2.java
@@ -0,0 +1,35 @@
+/*
+Program 2: Write a Java Program to find whether a string is palindrome or not.
+Sample Input:
+Java
+Sample Output:
+Not Palindrome
+
+Sample Input:
+Malayalam
+Sample Output:
+Palindrome
+
+Code:
+*/
+import java.util.Scanner;
+public class Program2 
+{
+	public static void main(String[] args) 
+{
+		Scanner scn = new Scanner(System.in);
+		String str=scn.nextLine(),rev="";
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			rev+=str.charAt(i);
+		}
+		if(rev.equalsIgnoreCase(str))
+		{
+			System.out.println("Palindrome");
+		}
+		else
+		{
+			System.out.println("Not Palindrome");
+		}
+	}
+}
