b8bc529a5108d663fd9a2cf13f61a3a077e3063d
dharnishp <128043459+dharnishp@users.noreply.github.com>
2025-02-08 20:24:34 +0530

    Update Program7.java

--- Program7.java
+++ Program7.java
@@ -1,13 +1,12 @@
 /*
  * Write A Java Program count the number of words in String .
  */
-package stringquestions;
+package com.dharnish.strings;
 
 import java.util.Scanner;
 
 public class Program7 
 {
-
 	public static void main(String[] args) 
 	{
 		Scanner scn=new Scanner(System.in);
