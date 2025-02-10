07ba34984ee4e46268d4d82c1ad366b85f70972b
dharnishp <128043459+dharnishp@users.noreply.github.com>
2025-02-10 19:37:06 +0530

    Add files via upload

---
+++ Program11.java
@@ -0,0 +1,24 @@
+/*
+ * Write A Java Program count the number of spaces in String .
+ */
+package stringquestions;
+
+import java.util.Scanner;
+
+public class Program11 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scn=new Scanner(System.in);
+		String str=scn.nextLine();
+		int count=0;
+		for(int i=0;i<str.length();i++)
+		{
+			if(str.charAt(i)==' ') 
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
---
+++ Program12.java
@@ -0,0 +1,44 @@
+/*
+ * Write A Java Program to check if two Strings are anagrams ?
+ */
+package stringquestions;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Program12 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scn=new Scanner(System.in);
+		String str1=scn.nextLine().toLowerCase(),str2=scn.nextLine().toLowerCase();
+		if(str1.length()==str2.length())
+		{
+			char[] ch1=str1.toCharArray();
+			char[] ch2=str2.toCharArray();
+			Arrays.sort(ch1);
+			Arrays.sort(ch2);
+			int count=0;
+			for(int i=0;i<ch1.length;i++)
+			{
+				if(ch1[i]==ch2[i])
+				{
+					count++;
+				}
+			}
+			if(count==str1.length())
+			{
+				System.out.println("Anagrams");
+			}
+			else
+			{
+				System.err.println("Not Anagrams");
+			}
+		
+		}
+		else
+		{
+			System.err.println("Not Anagrams");
+		}
+	}
+}
