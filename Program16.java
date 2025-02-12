beefdb4adf2fe2195b3b5b5333432fe05af04278
dharnishp <128043459+dharnishp@users.noreply.github.com>
2025-02-12 21:57:50 +0530

    Add files via upload

---
+++ Program16.java
@@ -0,0 +1,28 @@
+/*
+ * Write A Java Program prints vowels and consonants of a given string ? 
+ */
+package stringquestions;
+
+import java.util.Scanner;
+
+public class Program16 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scn=new Scanner(System.in);
+		String str=scn.nextLine();
+		String vow="",cons="";
+		for(int i=0;i<str.length();i++)
+		{
+			char ch=str.charAt(i);
+			if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U') {
+				vow+=ch;
+			}
+			else
+			{
+				cons+=ch;
+			}
+		}
+		System.out.println("Vowels in given String : "+vow+"\nConsonents in given String : "+cons);
+	}
+}
