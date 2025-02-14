f8fe10a5960a1878ee21eaf16df71dee8bedcc36
dharnishp <128043459+dharnishp@users.noreply.github.com>
2025-02-14 21:47:58 +0530

    Add files via upload

---
+++ Program20.java
@@ -0,0 +1,40 @@
+/*
+ * Write A Java Program to  find the first non-repeating character in a given String?
+ */
+package stringquestions;
+
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Program20 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scn=new Scanner(System.in);
+		String str1=scn.nextLine();
+		char[] ch=str1.toCharArray();
+		LinkedHashMap<Character,Integer>lhm=new LinkedHashMap();
+		for(Character c:ch)
+		{
+			if(lhm.containsKey(c))
+			{
+				lhm.put(c, lhm.get(c)+1);
+			}
+			else
+			{
+				lhm.put(c, 1);
+			}
+		}
+		System.out.println(lhm);
+		Set<Character> s=lhm.keySet();
+		for(Character c:s)
+		{
+		if(lhm.get(c)==1)
+		{
+			System.out.println(c);
+			break;
+		}
+		}
+	}
+}
